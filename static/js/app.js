754b75015637c55c2bfa1978792586f46d523109
brickleq <pblequire@gmail.com>
2019-06-09 18:06:38 -0500

    metadata panel and bubble chart working for all samples

--- static/js/app.js
+++ static/js/app.js
@@ -32,7 +32,7 @@ var url = "/samples/" + sample;
 var sampleName = sample;
 d3.json(url).then(function(sample) {
   var sampleData = sample;
-  console.log(sampleData);
+  console.log(data);
 
   // * Create a Bubble Chart that uses data from your samples route (`/samples/<sample>`) to display each sample.
 
@@ -45,18 +45,24 @@ d3.json(url).then(function(sample) {
 //   * Use `otu_ids` for the marker colors
 
 //   * Use `otu_labels` for the text values
-
+  var normalized_otu_ids = normalize_array(sampleData.otu_ids);
   var selector = d3.select("#bubble");
   selector.html("");
   var trace = {
-    x: [sampleData.otu_ids],
-    y: [sampleData.sample_values],
+    x: sampleData.otu_ids,
+    y: sampleData.sample_values,
+    mode: "markers",
     marker: {
-      size: [sampleData.sample_values],
-      colors: [sampleData.otu_ids]
+      size: sampleData.sample_values,
+      colorscale: "Earth",
+      color: normalized_otu_ids,
+      symbol: "circle"
     },
-    text: [sampleData.otu_labels]
+    text: sampleData.otu_labels,
+    type: "scatter"
   };
+  console.log(sampleData.otu_ids);
+  console.log(normalized_otu_ids);
   var data = [trace];
   console.log(data);
   var layout = {
@@ -67,7 +73,7 @@ d3.json(url).then(function(sample) {
   };
   var myDiv = document.getElementById("bubble");
   console.log([trace]);
-  Plotly.plot(myDiv, data, layout);
+  Plotly.newPlot(myDiv, data, layout);
 
 })
 
@@ -104,5 +110,24 @@ function optionChanged(newSample) {
   buildMetadata(newSample);
 }
 
+function normalize_array(arr) {
+// Normalize array values on scale of 0 to 1 (for color scale)
+  normalize = function(val, max, min) { 
+    return(val - min) / (max - min); 
+  }
+  max = Math.max.apply(null, arr) 
+  min = Math.min.apply(null, arr)
+  hold_normed_values=[]
+  arr.forEach(function(this_num) {
+    hold_normed_values.push(normalize(this_num, max, min))
+  })
+  return(hold_normed_values)
+}
+
+function event_listener() {
+  var selector = d3.select("#selDataselect");
+  selector.addEventListener("change",optionChanged);
+}
 // Initialize the dashboard
 init();
+event_listener();
